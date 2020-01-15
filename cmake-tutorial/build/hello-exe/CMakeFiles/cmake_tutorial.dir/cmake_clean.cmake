72a9abf4d80e60373604aed17f83bf60c4fb2099
hypark <HayeonP>
2020-01-15 14:48:00 +0900

    updated

---
+++ cmake-tutorial/build/hello-exe/CMakeFiles/cmake_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_tutorial.dir/main.cpp.o"
+  "cmake_tutorial"
+  "cmake_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
